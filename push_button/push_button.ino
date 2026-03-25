7d1502bad88601418946b4a3580b9c622d76fbc8
Vamshi-Shette <vamshishette@gmail.com>
2026-03-25 18:13:11 +0530

    push_button

---
+++ push_button/push_button.ino
@@ -0,0 +1,21 @@
+const int buttonPin =0;
+
+void setup() 
+{
+  Serial.begin(115200);
+  pinMode(buttonPin, INPUT_PULLUP);
+}
+
+void loop() {
+  if (digitalRead(buttonPin) == LOW) {   
+    delay(50);                           
+
+    if (digitalRead(buttonPin) == LOW) 
+    { 
+      Serial.println("Button Pressed");
+
+      while (digitalRead(buttonPin) == LOW);
+      delay(50); 
+    }
+  }
+}
